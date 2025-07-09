a01986be44a2bab5e0c5999853b9ff35ad211ed6
Payal Jain <payaljain@192.168.1.7>
2025-07-09 14:08:11 +0530

    Added failing test for - to pass the single input integer

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,12 @@ describe StringCalculator
         expect(StringCalculator.add("")).to eq(0)
       end
     end
+
+    context "given '7'" do
+      it "returns 7" do
+        expect(StringCalculator.add("7")).to eql(7)
+      end
+    end
+
   end
 end
